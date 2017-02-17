605df8531972160ce5dbcb4c962437a59444b93b
Abhinav Choudhury <abhinav155.bits@gmail.com>
2017-02-17 01:29:29 -0500

    Locality using Google places

---
+++ locality.py
@@ -0,0 +1,45 @@
+import json
+import urllib2
+
+header = {
+	"key" : "AIzaSyBejPwFdHsPVBdgUflKQ9v4rJbphmqh1k0",
+	"radius" : "1000"
+}
+
+f = open("train.json", "r")
+data = json.load(f)
+
+lat = data["latitude"]
+lon = data["longitude"]
+
+def send_request(header, _type):
+	baseURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+	header["type"] = _type
+	for param in header:
+		baseURL = baseURL + param + "=" + header[param] + "&"
+	response = urllib2.urlopen(baseURL).read()
+	results = json.loads(response)["results"]
+	filtered = []
+	for res in results:
+		d = {}
+		d["location"] = res["geometry"]["location"]
+		d["name"] = res["name"]
+		d["rating"] = res["rating"] if "rating" in res else None
+		d["types"] = res["types"] if "types" in res else None
+		filtered.append(d)
+	return(filtered)
+
+_place_types = ["subway_station", "transit_station", "train_station", "bus_station", "taxi_stand", "shopping_mall", "restaurant", "convenience_store", "department_store", "park", "night_club", "bar", "airport", "atm", "university"]
+
+content = []
+for key in lat:
+	print("Key: " + str(key) + "...")
+	loc = str(lat[key]) + "," + str(lon[key])
+	header["location"] = loc
+	_place_listings = {}
+	for _type in _place_types:
+		print(_type)
+		out = send_request(header, _type)
+		_place_listings[_type] = out
+	content.append({ key : _place_listings })
+	print("...Done")
