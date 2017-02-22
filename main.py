47333f0845c79a4165595a01385d95ec5ade9c9a
Abhinav <you@example.com>
2017-02-21 21:06:41 -0500

    Restructuring

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import numpy as np
 from gensim.models.doc2vec import TaggedDocument, Doc2Vec
@@ -6,41 +7,45 @@ from random import sample
 from sklearn.linear_model import LogisticRegression as lr
 from sklearn.metrics import log_loss
 
+_testcase = sys.argv[1]
+
 print("Loading data...")
 with open("train.json", "r") as f:
 	d = json.load(f)
 
 ### PART 1: Extract Doc2Vec features and represent description using that feature vector
+def desc2vector(d):
+	description = d["description"]
+	description = [description[key].encode('ascii', 'ignore') for key in description]
+	description = ["".join([ch for ch in article if (ch.isalnum() or ch.isspace())]) for article in description]	# Description content filtered off non-alphanum chars
 
-description = d["description"]
-description = [description[key].encode('ascii', 'ignore') for key in description]
-description = ["".join([ch for ch in article if (ch.isalnum() or ch.isspace())]) for article in description]	# Description content filtered off non-alphanum chars
-
-tagged = []
-for i in range(0, len(description)):
-	words = [word.strip() for word in description[i].split() if word.strip() != ""]
-	t = TaggedDocument(words=words, tags=["SENT_" + str(i)])
-	tagged.append(t)
+	tagged = []
+	for i in range(0, len(description)):
+		words = [word.strip() for word in description[i].split() if word.strip() != ""]
+		t = TaggedDocument(words=words, tags=["SENT_" + str(i)])
+		tagged.append(t)
 
 # Train the Doc2Vec model
-numFeatures = 100
-model = Doc2Vec(size=numFeatures, alpha= 0.025, min_alpha=0.025, window=8, min_count=5, workers=4)
-print("Building vocab...")
-model.build_vocab(tagged)
-for epoch in range(5):
-	print("Training epoch " + str(epoch) + "...")
-	model.train(tagged)
-	model.alpha -= 0.002
-	model.min_alpha = model.alpha
+	numFeatures = 100
+	model = Doc2Vec(size=numFeatures, alpha= 0.025, min_alpha=0.025, window=8, min_count=5, workers=4)
+	print("Building vocab...")
+	model.build_vocab(tagged)
+	for epoch in range(5):
+		print("Training epoch " + str(epoch) + "...")
+		model.train(tagged)
+		model.alpha -= 0.002
+		model.min_alpha = model.alpha
 
-labels = ["SENT_" + str(i) for i in range(len(description))]
-vecs = [list(i) for i in model.docvecs[labels]]
+	labels = ["SENT_" + str(i) for i in range(len(description))]
+	vecs = [list(i) for i in model.docvecs[labels]]
 
-# Create a DataFrame of the vector representation of the description
-df = pd.DataFrame(vecs, columns = ["F_" + str(i) for i in range(numFeatures)])
-
+	# Create a DataFrame of the vector representation of the description
+	df = pd.DataFrame(vecs, columns = ["F_" + str(i) for i in range(numFeatures)])
+	return(df)
 
 ## PART 2: Construct the full DataFrame
+df = desc2vector(d)
+
 price = [d["price"][key] for key in d["price"]]
 bedrooms = [d["bedrooms"][key] for key in d["bedrooms"]]
 bathrooms = [d["bathrooms"][key] for key in d["bathrooms"]]
